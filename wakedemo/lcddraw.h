9eeae3e29d8ec2d764cf9114360806d010c74792
maruvalcaba <maruvalcaba@miners.utep.edu>
2020-11-17 15:12:38 -0700

    Added the new function to the lcddraw.h file and added a new dependency to the Makefile

--- wakedemo/lcddraw.h
+++ wakedemo/lcddraw.h
@@ -60,6 +60,31 @@ void drawChar5x7(u_char col, u_char row, char c,
  */
 void drawRectOutline(u_char colMin, u_char rowMin, u_char width, u_char height,
 		     u_int colorBGR);
+
+
+/** Draw string at col,row
+ *  Type:
+ *  FONT_SM - small (5x8,) FONT_MD - medium (8x12,) FONT_LG - large (11x16)
+ *  FONT_SM_BKG, FONT_MD_BKG, FONT_LG_BKG - as above, but with background color
+ *  Adapted from RobG's EduKit
+ *
+ *  \param col Column to start drawing string
+ *  \param row Row to start drawing string
+ *  \param string The string
+ *  \param fgColorBGR Foreground color in BGR
+ *  \param bgColorBGR Background color in BGR
+ */
+void drawString8x12(u_char col, u_char row, char *string, 
+		   u_int fgColorBGR, u_int bgColorBGR);
+
+/** 5x7 font - this function draws background pixels
+ *  Adapted from RobG's EduKit
+ */
+void drawChar8x12(u_char col, u_char row, char c, 
+		 u_int fgColorBGR, u_int bgColorBGR);
+
+
+void drawDiamond(u_char offc, u_char offr, u_int color);
 #endif // included
 
 
